0c7fb15c176c1c8b5ec95fa0e6e66ab4d2acaa35
Jarrod Medrano <jmedran@gmail.com>
2019-02-17 19:13:19 -0700

    adding express handlebars helpers

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,14 @@ app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
 
-app.set('.hbs', exphbs({
+app.engine('.hbs', exphbs({
     extname: '.hbs',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./lib/helpers')
 }));
+app.set('view engine', '.hbs');
+
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
