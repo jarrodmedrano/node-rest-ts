37e942286dd91846898a4e70a84f119194909633
Jarrod Medrano <jmedran@gmail.com>
2019-02-17 19:11:36 -0700

    adding express handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';
 
 //Initializations
 const app = express();
 
 //Settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('.hbs', exphbs({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+}));
 //Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //Routes
+
 
 //Static Files
 
