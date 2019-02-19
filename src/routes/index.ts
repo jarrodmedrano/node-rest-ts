9f28ab8819d8244095adbb1b7e1a4713269b58a6
Jarrod Medrano <jmedran@gmail.com>
2019-02-18 19:22:39 -0700

    adding about route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,11 @@ const router: Router = Router();
 
 router.get('/', indexController.index);
 
-
 router.get('/add',(req,res) => {
     res.send('Form');
 });
+
+router.get('/about',(req,res) => res.render('about'));
 
 export default router;
 
